ec54026cd7dfe3eb034f8b67428a0932bfa7cdd0
tjdanielson <tessajdanielson@gmail.com>
2022-03-02 17:00:38 -0600

    Create skeleton for CellPhone class.

---
+++ CellPhone.py
@@ -0,0 +1,20 @@
+
+class CellPhone:
+    
+    def __init__(self, model):
+        self.model = model,
+        self.phone_number = '555-555-5555'
+        self.contacts = []
+        self.messages = []
+        self.vibrate_on = True
+    
+    def receive_text(self):
+        pass
+
+    def toggle_vibrate(self):
+        pass
+
+    def send_text(self):
+        pass
+    
+
