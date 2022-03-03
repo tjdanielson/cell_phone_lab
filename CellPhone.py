8a760771b8aeb672492f0cedd40f1b63367d3f21
tjdanielson <tessajdanielson@gmail.com>
2022-03-03 08:26:41 -0600

    Build out toggle_vibrate and send_text methods

--- CellPhone.py
+++ CellPhone.py
@@ -8,15 +8,24 @@ class CellPhone:
         self.messages = []
         self.vibrate_on = True
     
-    def receive_text(self, message):
-        message = input('Enter text to send a message to the Cell Phone')
-        self.messages.append(message)
-        print('New Text Received: ' + message)
+    def receive_text(self, message_received):
+        message_received = input('Enter text to send a message to the Cell Phone')
+        self.messages.append(message_received)
+        print('New Text Received: ' + message_received)
 
     def toggle_vibrate(self):
-        pass
+        if self.vibrate_on == True:
+            self.vibrate_on = False
+        else:
+            self.vibrate_on = True
 
-    def send_text(self):
-        pass
+    def send_text(self, message_sent):
+        print('Contacts: ')
+        for i in self.contacts:
+            print(i)
+        contact = input('Choose an above contact to send a message to')
+        message_sent = input('Enter text to send a message from the Cell Phone')
+        print(f'Message to {contact} sent. Message: {message_sent}')
+
 
 
